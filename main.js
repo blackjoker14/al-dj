6b8c0b0a178f93ae1995e626a9b78fdd0d17cfd3
blackjoker14 <86548199+blackjoker14@users.noreply.github.com>
2022-11-12 10:59:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left wrist x =" + leftWristX + "left wrist y =" + leftWristY);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
